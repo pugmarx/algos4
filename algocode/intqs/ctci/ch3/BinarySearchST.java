02e62550728e05b32c082e0394f1767fd4e68477
pugmarx <naqi.rizvi@gmail.com>
2018-03-03 23:27:08 +0530

    Basic BinarySearchST impl. from Sedgewick et. al :: brokencd add .

---
+++ algocode/intqs/ctci/ch3/BinarySearchST.java
@@ -0,0 +1,135 @@
+
+/**
+	Binary Search ST from Sedgewick et. al.
+
+	// FIXME Broken!! Duplicate keys getting inserted. See put() logic!!
+**/
+
+public class BinarySearchST<Key extends Comparable<Key>, Value>{
+
+
+	private Key[] keys;
+	private Value[] vals;
+	private int N;
+
+	public BinarySearchST(int capacity){
+		keys = (Key[]) new Comparable[capacity];
+		vals = (Value[]) new Object[capacity];	
+	}
+	
+	
+	public Value get(Key key){
+		if(N == 0) return null;
+		int index = rank(key);
+
+		if(index < N && keys[index].compareTo(key) ==0){
+			return vals[index];
+		}
+
+		return null;
+	}
+
+	public void put(Key key, Value val){
+		int index = rank (key);
+
+		// update if already exists
+		if(keys[index] == null){
+			keys[index] = key;
+			vals[index] = val;
+			N++;
+			return;
+		}else{
+			if(index < N && keys[index].compareTo(key) == 0){
+				vals[index] = val;
+				return;
+			}
+
+			// create space for this new key
+			for(int i = N; i > index; i--){
+				keys[i] = keys[i-1];
+				vals[i] = vals[i-1];
+
+				keys[index] = key;
+				vals[index] = val;
+			}		
+			N++;
+		}
+	}
+
+	// iterative rank impl.
+	public int rank(Key key){
+		int lo = 0;
+		int hi = N-1; // size = 3 => 0..2 
+	
+		while (lo <= hi){
+			int mid = lo + (hi - lo)/2;
+			System.out.println(lo + "::" + hi+ "::"+mid);
+
+			int cmp = key.compareTo(keys[mid]);
+
+			if(cmp == 0) return mid;
+			if(cmp < 0){ // search left half
+				hi = mid-1;
+			}else if(cmp > 0){		// search right half
+				lo = mid+1;
+			}else{ 
+				return mid;
+			}
+		}
+		return lo; // !!
+	}
+
+
+	// recursive rank impl.
+	public int rankr(Key key){
+		return rankr(key, 0, N-1);
+	}
+
+	// retursive rank impl.
+	private int rankr(Key key, int lo, int hi){
+		if(hi < lo) return lo;	
+		int mid = lo + (hi - lo)/2;
+		int cmp = keys[mid].compareTo(key);
+		
+		if(cmp == 0) return mid;
+
+		if(cmp > 0){ // look in left-half
+			return  rankr(key, lo, mid - 1);
+		}else{		// look in right-half
+			return rankr(key, mid +1, hi);
+		}
+	}
+
+
+	public void print(){
+		for(int i =0;i< N;i++){
+			System.out.print(keys[i]+ "|\t");
+		}
+		
+		System.out.println();
+		for(int i =0;i< N;i++){
+			System.out.print(vals[i]+ "|\t");
+		}
+		System.out.println();
+	}
+	
+	public void delete(Key key){}
+
+	public static void main(String a[]){
+		BinarySearchST<String, Integer> bsst = new BinarySearchST<>(100);
+		bsst.put("S", 1);	
+		bsst.put("E", 2);	
+		bsst.put("A", 3);	
+		bsst.put("R", 4);	
+		bsst.put("C", 5);	
+		bsst.put("H", 6);	
+		bsst.put("E", 7);	
+		bsst.put("X", 8);	
+		bsst.put("A", 9);	
+		bsst.put("M", 10);	
+		bsst.put("P", 11);
+
+		bsst.print();	
+	}
+
+}
